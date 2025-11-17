ca10b76343892db5fca00cde04e1755499964259
0xMoncif <m_fokroun@estin.dz>
2025-11-17 21:20:53 +0100

    registeration finished

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,12 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser) :
-    USER_TYPE_CHOICES = {
+    USER_TYPE_CHOICES =( 
         ('student', 'Student'),
         ('company', 'Company'),
-    }
+    )
 
     first_name = None
     last_name = None
@@ -15,5 +31,7 @@ class User(AbstractUser) :
     user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
