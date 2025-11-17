d94c9d7d03e57197e302607c757a8405a9826a66
0xMoncif <m_fokroun@estin.dz>
2025-11-17 18:55:54 +0100

    added the User model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser) :
+    USER_TYPE_CHOICES = {
+        ('student', 'Student'),
+        ('company', 'Company'),
+    }
+
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
